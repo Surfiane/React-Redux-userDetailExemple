35def6a14e97511459a0192660456483598b3722
Soufiane <soufiane.ouirane@acaisoft.com>
2017-05-04 12:51:24 +0200

    Add reducer-user.js where is user data and pass it to reducers/index.js where all reduces combine then pass it to the store in js/index.js

---
+++ dev/js/reducers/index.js
@@ -0,0 +1,7 @@
+import {combineReducers} from 'redux';
+import UserReducer from './reducer-user';
+
+
+const allReducers = combineReducers({
+    users:  UserReducer
+});
---
+++ dev/js/reducers/reducer-user.js
@@ -0,0 +1,34 @@
+export default function () {
+    return [
+        {
+            id: 1,
+            firstName: "Adil",
+            lastName: "Robert",
+            age: 43,
+            description: "Adil he is a Sport man likes to enjoy the life... ",
+            thumbnail: "http://dp.topcovers4fb.com/wp-content/uploads/2015/10/Pleasant-Smile-Cute-Baby-Display-Picture.jpg"
+        },
+        {
+            id: 2,
+            firstName: "Amine",
+            lastName: "Ben",
+            age: 30,
+            description: "Amine he is an Adected coder ",
+            thumbnail: "https://s-media-cache-ak0.pinimg.com/564x/e6/53/77/e65377233fcfc7eac0a71264036b0505.jpg"
+        }, {
+            id: 3,
+            firstName: "Soufiane",
+            lastName: "Oui",
+            age: 31,
+            description: "Soufiane he is an developer likes coding and extreme sports ",
+            thumbnail: "http://dp.topcovers4fb.com/wp-content/uploads/2015/12/Boy-Surprise-Display-Picture.jpg"
+        }, {
+            id: 4,
+            firstName: "Akram",
+            lastName: "Kiteman",
+            age: 30,
+            description: "He is a kite surfer ",
+            thumbnail: "http://dp.topcovers4fb.com/wp-content/uploads/2015/11/Cute-Sleeping-Toy-Baby-Display-Picture.jpg"
+        }
+    ]
+}
